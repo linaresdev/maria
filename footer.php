8042dba2622ee12fe08f8b758df21937d66914d9
Ramon Linares <linareslf@gmail.com>
2024-10-13 16:09:42 -0400

    Avances pendientes

--- footer.php
+++ footer.php
@@ -1,3 +1,27 @@
-<p>Footer</p>
+
+	<footer class="container-fluid bg-secondary text-white">
+		<section class="container py-3">
+			<article class="row">
+				<section class="col-lg-3 col-md-6 col-sm-12">
+					<h4>Title</h4>
+				</section>
+				<section class="col-lg-3 col-md-6 col-sm-12">
+					<h4>Redes</h4>
+				</section>
+				<section class="col-lg-3 col-md-6 col-sm-12">
+					<h4>Mapa</h4>
+				</section>
+				<section class="col-lg-3 col-md-6 col-sm-12">
+					<h4>Mapa</h4>
+				</section>
+			</article>
+		</section>
+		<section class="row bg-dark">
+			<article style="font-size:12px;" class="col py-2 text-center">
+				C/ 2da #2, Res. Estrella del Este II, Cabirma del Este, Apdo Postal 11513. Santo Domingo. Este. República
+Dominicana Tel:. 1 809 273 5371 | 1 809 861 0499
+			</article>
+		</section>
+	</footer>
 </body>
 </html>
--- functions.php
+++ functions.php
@@ -8,3 +8,41 @@
 */
 
 
+## HELPERS
+if( !function_exists("maria_asset_css") )
+{
+	function maria_asset_css($path=null)
+	{
+		return get_template_directory_uri()."/assets/css/".trim($path, '/');
+	}
+}
+if( !function_exists("maria_asset_js") )
+{
+	function maria_asset_js($path=null)
+	{
+		return get_template_directory_uri()."/assets/js/".trim($path, '/');
+	}
+}
+
+
+## STYLE && JS 
+function mariamentor_styles()
+{
+	wp_enqueue_style(
+		'bootstrap.css', maria_asset_css("bootstrap.min.css"), array(), "5.3.3"
+	);
+	wp_enqueue_style(
+		'mdi', maria_asset_css("materialdesignicons.min.css"), array(), "5.3.3"
+	);
+	wp_enqueue_style(
+		'layout', maria_asset_css("layout.css"), array(), "1.0"
+	);
+	wp_enqueue_script(
+		"layout.js", maria_asset_js("layout.js"), array('jquery'), "1.0"
+	);
+	wp_enqueue_script(
+		"bootstrap.js", maria_asset_js("bootstrap.bundle.min.js"), array('jquery'), "1.0"
+	);
+}
+
+add_action('wp_enqueue_scripts', "mariamentor_styles");
